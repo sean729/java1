ed61710cede722414122aca341f30a694ce51422
Sean Peterson <sean729@gmail.com>
2020-03-15 20:01:07 -0500

    fixed tests commit

--- test/java/sean/p1/service/IGVServiceImplTest.java
+++ test/java/sean/p1/service/IGVServiceImplTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reto.intercorp.renzocarpio.service;
+package reto.sean.p1.service;
 
 import io.micronaut.test.annotation.MicronautTest;
 import org.junit.jupiter.params.ParameterizedTest;
